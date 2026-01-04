f18d06c2d240da7938b8b6f3575d01092e15f2d2
Mudrichenko <e.mudrichenko@astondevs.ru>
2026-01-04 22:41:47 +0300

    build: setup maven central publishing with gpg signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,12 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     `maven-publish`
+    signing
 }
 
 allprojects {
-    group = "com.mudrichenkoevgeny.backend"
-    version = "1.0.0-SNAPSHOT"
+    group = "io.github.mudrichenko-evgeny"
+    version = "0.0.1"
 
     repositories {
         mavenCentral()
@@ -16,7 +17,15 @@ allprojects {
 }
 
 subprojects {
+    apply(plugin = "java")
+    apply(plugin = "kotlin")
     apply(plugin = "maven-publish")
+    apply(plugin = "signing")
+
+    configure<JavaPluginExtension> {
+        withSourcesJar()
+        withJavadocJar()
+    }
 
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
@@ -24,12 +33,54 @@ subprojects {
         }
     }
 
-    publishing {
+    configure<PublishingExtension> {
         publications {
             create<MavenPublication>("maven") {
-                from(components.findByName("java"))
+                from(components["kotlin"])
                 artifactId = project.name
+
+                pom {
+                    name.set("Backend SDK - ${project.name}")
+                    description.set("Module ${project.name} of the backend-platform-sdk")
+                    url.set("https://github.com/mudrichenko-evgeny/backend-platform-sdk")
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("mudrichenkoevgeny")
+                            name.set("Evgeny Mudrichenko")
+                            email.set("evgeny.mudrichenko@gmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:git://github.com/mudrichenko-evgeny/backend-platform-sdk.git")
+                        developerConnection.set("scm:git:ssh://github.com/mudrichenko-evgeny/backend-platform-sdk.git")
+                        url.set("https://github.com/mudrichenko-evgeny/backend-platform-sdk")
+                    }
+                }
             }
+        }
+
+        repositories {
+            maven {
+                name = "OSSRH"
+                url = uri("https://central.sonatype.com/api/v1/publisher/upload")
+                credentials {
+                    username = project.property("mavenCentralUsername").toString()
+                    password = project.property("mavenCentralPassword").toString()
+                }
+            }
+        }
+    }
+
+    configure<SigningExtension> {
+        val isSigningKeyPresent = project.hasProperty("signing.keyId")
+        if (isSigningKeyPresent) {
+            sign(extensions.getByType<PublishingExtension>().publications["maven"])
         }
     }
 }
